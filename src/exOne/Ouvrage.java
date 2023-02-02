ab37aa7a36b1d5935f144056de250490cd2a0dd0
JourdainK <K.jourdain@condorcet.be>
2023-02-02 18:46:40 +0100

    first commit

--- src/exOne/Ouvrage.java
+++ src/exOne/Ouvrage.java
@@ -52,4 +52,35 @@ public abstract class Ouvrage {
     public void setListAuteurs(List<Auteur> listAuteurs) {
         this.listAuteurs = listAuteurs;
     }
+
+    //check if author is in listAuteurs ==> return true if author is found in listAuteurs
+    public boolean isAuthorIn(Auteur author){
+        boolean checkAuthor = false;
+
+        for(Auteur m:listAuteurs){
+            if(author.equals(m)){
+                checkAuthor = true;
+            }
+        }
+        return checkAuthor;
+    }
+
+    //add method that'll add one author to the List of authors if it isn't in the List 'listAuteurs'
+    public void addAuthor(Auteur auteur){
+        boolean checkAddAuthor;
+
+        checkAddAuthor = isAuthorIn(auteur);
+        if(checkAddAuthor){
+            System.out.println("L'auteur est déja présent dans la liste");
+        }
+        else{
+            listAuteurs.add(auteur);
+            System.out.println(auteur.getNom() + "à été ajouté à la liste des auteurs de " + titre + ".");
+        }
+    }
+
+
+
+
+
 }
