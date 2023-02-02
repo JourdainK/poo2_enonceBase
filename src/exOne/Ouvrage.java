88e3ae1db0b73e0968c31794af36c56ed5fdb8ab
JourdainK <K.jourdain@condorcet.be>
2023-02-02 22:08:33 +0100

    hesitation about composition

--- src/exOne/Ouvrage.java
+++ src/exOne/Ouvrage.java
@@ -2,6 +2,7 @@ package exOne;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public abstract class Ouvrage {
     protected String titre, dateParution, langue, genre, typeOuvrage;
@@ -77,6 +78,43 @@ public abstract class Ouvrage {
             listAuteurs.add(auteur);
             System.out.println(auteur.getNom() + "à été ajouté à la liste des auteurs de " + titre + ".");
         }
+    }
+
+    /*
+    had hesitation about whether to created "Exemplaire" inside or outside "Ouvrage"
+    => After asking chatGpt : chosen to create it outside "Ouvrage" for more flexibility
+    ?? make the reference null after having created the "Exemplaire" => so the only ref is within "Ouvrage"
+     */
+    //add a copy to the List "listExemplaire" => return true > success
+    public boolean addExemplaire(Exemplaire copy){
+        Exemplaire exTmp;
+        boolean isCopyIn,isCopyAdded;
+        Scanner sc = new Scanner(System.in);
+        isCopyIn = isExemplaireIn(copy);
+
+        if(isCopyIn){
+            isCopyAdded = false;
+        }
+        else{
+            listExemplaire.add(copy);
+            System.out.println("Exemplaire ajouté.");
+            isCopyAdded = true;
+        }
+
+        return isCopyAdded;
+    }
+
+    //check if copy is in listExemplaire > return true if found
+    public boolean isExemplaireIn(Exemplaire copy){
+        boolean checkIfIsIn = false;
+
+        for(Exemplaire c:listExemplaire){
+            if(copy.equals(c)){
+                checkIfIsIn = true;
+            }
+        }
+
+        return checkIfIsIn;
     }
 
 
