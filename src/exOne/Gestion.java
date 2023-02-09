f9a7f67a3d6e980afa9de9c345fa4a9a0b451c76
JourdainK <K.jourdain@condorcet.be>
2023-02-09 09:36:28 +0100

    exercice classroom

---
+++ src/exOne/Gestion.java
@@ -0,0 +1,7 @@
+package exOne;
+
+public class Gestion {
+    public static void main(String[] args) {
+
+    }
+}
--- src/exOne/Main.java
+++ src/exOne/Main.java
@@ -7,8 +7,6 @@ import java.time.format.DateTimeFormatter;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("No time to waste !");
 
-        System.out.println("Obladi obla da ! life goes on");
     }
 }
